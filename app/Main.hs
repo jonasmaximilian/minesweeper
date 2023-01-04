57a7db7e3c6fb344a54bbc7e78d195c91a55ca5a
Jonas Maximilian Senft-Raiss <jonasmas@tcd.ie>
2023-01-04 14:18:48 +0100

    added minesweeper grid without any functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,33 @@
 module Main (main) where
 
 import Lib
---import qualified Graphics.UI.Threepenny as UI
---import Graphics.UI.Threepenny.Core
+import qualified Graphics.UI.Threepenny       as UI
+import           Graphics.UI.Threepenny.Core
+import           Control.Monad                (forM_, forM)
+--import Text.XHtml (HTMLTABLE(cell))
 
 main :: IO ()
-main = do 
-    putStrLn "Hello, Haskell!"
-    let board = initBoard 1 1
-    printBoard board
-    board' <- insertMines 0 board
-    printBoard board'
-    print $ gameWon board'
-    print $ gameLost board'
-    putStrLn "Uncovering 0,0"
-    -- uncover and save the new board
-    board <- return $ uncover 0 0 board'
-    printBoard board
-    print $ gameWon board
-    print $ gameLost board
+main = do
+    startGUI defaultConfig
+        { jsPort       = Just 8023
+        , jsStatic     = Just "../wwwroot"
+        } setup
+
+setup :: Window -> UI ()
+setup window = do
+    return window # set UI.title "Minesweeper"
+    board <- UI.table 
+
+    forM_ [0..9] $ \row -> do 
+        rowElem <- UI.tr
+        forM_ [0..9] $ \col -> do
+            cell <- UI.td # set UI.text " "
+            element cell # UI.set UI.style [("border", "1px solid black"), ("width", "30px"), ("height", "30px")]
+            getBody window #+ [element cell]
+        getBody window #+ [element rowElem]
+        getBody window #+ [element board]
+
+
+
     
-
-
-
---main = putStrLn "Hello, Haskell!"
+    
